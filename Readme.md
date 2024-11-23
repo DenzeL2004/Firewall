86a55202c2fb75b76b51192d033de728838c2d2d
DenzeL2004 <work20022004@mail.ru>
2024-11-24 02:13:24 +0300

    nfqueue

--- Readme.md
+++ Readme.md
@@ -12,60 +12,18 @@
 
 Для запуска программы введите
 ```
-python3 filter.py rule interface1 interface2
+python3 filter.py rule --queue-num
 ```
-rule - путь до файла, содержащий правила фильтрации
-interface1, interface2 - имена интерфейсов, которые будет прослушивать фильтр.
+rule - путь до файла, содержащий правила фильтрации.
 
 
 ## Правила
 Формат файла с правилам:
 ```
-{
-   type: [black, white] 
-   rules:
-   {
-       {
-           prot: [udp, tcp, icmp]
-           srcIP: xxx.xxx.xxx.xxx
-           dstIP: xxx.xxx.xxx.xxx
-           srcPort: num
-           dstPort: num
-       },
-       ...
-   }
-}
-```
-
-
-_black_ - пропускает все пакеты, кроме тех, которые удовлетворяют хотя бы одному правилу.
-_white_ - пропускает только те пакеты, которые удовлетворяют хотя бы одному правилу.
-
+action={0,1} DNS QR={0,1} name=regx type=num class=num len=num data=regx
 
 __Пример:__
 ```
-{
-   type: black,
-   rules:
-   {
-       {
-           prot: udp
-           srcIP: 192.168.0.1,
-           dstIP: 123.123.123.123
-           dstPort: 3000
-       },
-       {
-           prot: icmp
-       },
-       {
-           prot: tcp
-           dstPort: 200
-       },
-       {
-           prot: tcp
-           dstPort: 201
-       }
-   }
-}
+DNS QR=0 name=ya.ru
 ```
 
--- filter.py
+++ filter.py
@@ -1,18 +1,26 @@
 import argparse
 import re 
-import os
+import enum
 import socket
 from socket import AF_PACKET, AF_INET, SOCK_RAW, ETH_P_ALL
-
+from netfilterqueue import NetfilterQueue
 import dpkt
 
 BUF_SZIE = 1024
+PROTOCOLS = ['DNS']
+PROTOCOLS_FLAGS =   {'DNS' : {  0 : ['name', 'type', 'class'],
+                                1 : ['name', 'type', 'class', 'len', 'data']
+                             }
 
-FILTER_COND = {'prot', 'srcIP', 'dstIP', 'srcPort', 'dstPort'} 
+                    }
+
+class Action(enum.Enum):
+    drop = 0
+    accept = 1
+    default = 2
 
 class RulesList:
-    def __init__(self, type : str):
-        self.is_white = (type == "white")
+    def __init__(self):
         self.rules =  list()
     
     def update(self, rule : dict) -> None:
@@ -20,66 +28,107 @@ class RulesList:
     
     def match(self, pkt, rule) -> bool:
         match = True
-        for key in FILTER_COND:
-            if (key in rule.keys()) and (key in pkt.keys()):
-                match &= (rule[key] == pkt[key])
-        
-        return match
+        for flag in pkt.keys():
+            if flag not in rule.keys():
+                continue
+            
+            if flag in ['name', 'data']:
+                match &= rule[flag].match(pkt[flag]) is not None
+            else:
+                match &= pkt[flag] == rule[flag]
+            
+            if not match: 
+                break
                 
-
+        if match:
+            return rule['action']
+        
+        return Action.default
+                
     def passed(self, pkt) -> bool:
-        match = False
+        match = Action.default
         for rule in self.rules:
             match = self.match(pkt, rule)
-            if match: 
+            if match == Action.drop or match == Action.accept: 
                 break
-        
-        return (not self.is_white and not match) or (self.is_white and match)
+            
+        return match
+    
+    def show_rules(self):
+        for rule in self.rules:
+            print(rule)
         
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("rules", type=str, help='Path to the file name with rules')
-    parser.add_argument('ifIn', type=str, help='Input interface')
-    parser.add_argument('ifOut', type=str, help='Output interface')
+    parser.add_argument("rules", type=str)
+    parser.add_argument('--queue-num', type=int, default=5)
     return parser.parse_args()
 
+def parse_cond(cond : str):
+    args = cond.strip().split(sep='=')
+    if len(args) != 2:
+        raise ("Uncorrect arguments, format must be \'flag=arg\'.\n")
+
+    return args[0], args[1]
+
 def define_rules(path : str) -> RulesList:
+    rules_list = RulesList()
+    
     with open(path, 'r') as file:
-        content = file.read()
-    
-    if content.find("white") != -1: 
-        rules_list = RulesList("white")
-    elif content.find("black") != -1:
-        rules_list = RulesList("black")
-    else:
-        raise Exception('Invalide type RuleList. Must be \'black\' or \'white\'.')
-
-    begin = content.find('{', content.find("rules") + 1)
-    while begin != -1:
-        begin = content.find('{', begin + 1)
-        if (begin == -1):
-                break
+        for line in file:
+            if len(line) == 0:
+                continue
             
-        end = content.find('}', begin)
-        if end == -1:
-            raise Exception("No closed \'}\'.")
-        
-        conditions = list(filter(None, re.split(r'[\s:,]', content[begin + 1: end - 1])))
-
-        rule = dict()
-        for i in range(0, len(conditions), 2):
-            if conditions[i] not in FILTER_COND:
-                raise Exception(f"Undefine condition:{conditions[i]}.")
+            conds = line.strip().split()
+            rule = dict()
             
-            if (conditions[i] == 'dstPort' or conditions[i] == 'srcPort'):
-                rule.update({conditions[i] : socket.htons(int(conditions[i + 1]))})
-            else:         
-                rule.update({conditions[i] : conditions[i + 1]})
-        
-        rules_list.update(rule)        
-    
+            flag, type = parse_cond(conds[0])
+            if flag != 'action':
+                raise Exception('First argument must be \'action\'=num.\n')
+            
+            if type not in ['0', '1']:
+                raise Exception('Action flag must be 0 or 1.')
+            
+            if type == '0':
+                rule.update({'action' : Action.drop})
+            else:
+                rule.update({'action' : Action.accept})
+            
+            prot = conds[1]
+            if prot not in PROTOCOLS:
+                raise Exception(f'Undefind prot={prot}.\n')
+            
+            rule.update({'prot' : prot})
+            
+            if prot == 'DNS':
+                if len(conds) < 2:
+                    raise Exception(f'Under DNS protocols, the second argument must always be the request type.\n')
+                
+                flag, type = parse_cond(conds[2])
+                if flag != 'QR':
+                    raise Exception("Under DNS protocols, the second argument must always be the request type.\n")
+                
+                type = int(type)
+                if type not in [0, 1]:
+                    raise Exception("Uncorrect QR value. Must be 0 or 1.\n")
+                rule.update({'QR' : int(type)})
+                
+                for i in range(3, len(conds)):
+                    flag, arg = parse_cond(conds[i])
+                    if flag not in PROTOCOLS_FLAGS['DNS'][type]:
+                        raise Exception(f"Undefine flag({flag})")
+
+                    if flag in ['name', 'data']:
+                        rule.update({flag : re.compile(arg)})
+                    
+                    if flag in ['type', 'class', 'length']:
+                        rule.update({flag : int(arg)})
+                    
+            rules_list.update(rule)              
+            
     return rules_list
+    
 
 def create_socket(if_name : str) -> socket:
     sock = socket.socket(AF_PACKET, SOCK_RAW)
@@ -91,65 +140,71 @@ def print_packet(pkt) -> None:
         print(f"{key}: {val}")
     
         
-def define_packet(eth : dpkt.ethernet.Ethernet) -> dict:
+def check_is_dns(data) -> bool:
+    eth = dpkt.ethernet.Ethernet(data)
     ip = eth.data
+    udp = ip.data
+    return isinstance(udp.data, dpkt.dns.DNS)
     
-    srcIP = str(socket.inet_ntop(AF_INET, ip.src))
-    dstIP = str(socket.inet_ntop(AF_INET, ip.dst))
+def get_dns_packet(data) -> dict:
+    ip = dpkt.ethernet.Ethernet(data).data
+    udp = ip.data
+    dns = dpkt.dns.DNS(udp.data)
     
-    if isinstance(ip.data, dpkt.tcp.TCP):
-        pkt = {'prot'    : 'tcp', 
-               'srcIP'   : srcIP, 
-               'dstIP'   : dstIP,
-               'srcPort' : socket.ntohs(ip.data.sport),
-               'dstPort' : socket.ntohs(ip.data.dport)}
-    
-    if isinstance(ip.data, dpkt.udp.UDP):
-        pkt = {'prot'    : 'udp', 
-               'srcIP'   : srcIP, 
-               'dstIP'   : dstIP,
-               'srcPort' : socket.ntohs(ip.data.sport),
-               'dstPort' : socket.ntohs(ip.data.dport)}
+    pkt = dict({"prot" : "DNS"})
+    if dns.qr == dpkt.dns.DNS_R:
+        rr_data, off = dns.unpack_rr(data, 0)
+        pkt.update({"QR"    : 1,
+                    "name"  : rr_data.name,
+                    "type"  : rr_data.type, 
+                    "class" : rr_data.cls, 
+                    "len"   : rr_data.rlaen, 
+                    "data"  : rr_data.rdata})
         
-    if isinstance(ip.data, dpkt.icmp.ICMP):
-        pkt = {'prot'    : 'icmp', 
-               'srcIP'   : srcIP, 
-               'dstIP'   : dstIP}
+    if dns.qr == dpkt.dns.DNS_Q:
+        q_data, off = dns.unpack_q(data, 0)
+        pkt.update({"QR"    : 0,
+                    "name"  : q_data.name,
+                    "type"  : q_data.type, 
+                    "class" : q_data.cls})
         
     return pkt
 
-def myfilter(input : socket, output : socket, rules : RulesList) -> bool:
-    data = input.recv(BUF_SZIE)
+def packet_handler(pkt, rules : RulesList):
+    data = pkt.get_payload()
     
-    eth = dpkt.ethernet.Ethernet(data)
-    if not isinstance(eth.data, dpkt.ip.IP):
+    if not check_is_dns(data):
         print("Skip package");
     else:
-        pkt = define_packet(eth)
+        pkt = get_dns_packet(data)
         print_packet(pkt)
         
-        if rules.passed(pkt):
-            print("Pass packet.")
+        act_type = rules.passed(pkt)
+        if act_type == Action.drop:
+            print("Drop")
+            return pkt.drop()
         else:
-            print("Drop packet.")
-            return False
-        
-    output.sendall(data)
-    return True
+            print("Accept")        
+
+    return pkt.accept()
         
 
 def main(args):
     rules_list = define_rules(args.rules)
     
-    input  = create_socket(args.ifIn)
-    output = create_socket(args.ifOut)
+    nfqueue = NetfilterQueue()
+    wrapper = lambda pkt: packet_handler(pkt, rules_list)
     
-    if os.fork():
-        while(True):
-            myfilter(input, output, rules_list)
-    else:
-        while(True):
-            myfilter(output, input, rules_list)
+    nfqueue.bind(int(args.queue_num), wrapper)
+    sock = socket.fromfd(nfqueue.get_fd(), socket.AF_UNIX, socket.SOCK_STREAM)
+    
+    try:
+        nfqueue.run_socket(sock)
+    except KeyboardInterrupt:
+        pass
+
+    sock.close()
+    nfqueue.unbind()
 
 if __name__ == "__main__":
     main(parse_args())
